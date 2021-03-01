2b03f7acb8fc0bd2182a50177678e6d57c7398f8
javorkabotond <botondjvorka@gmail.com>
2021-03-01 19:47:40 +0100

    Laravel-Blade templating engine

--- laravel/resources/views/contact.blade.php
+++ laravel/resources/views/contact.blade.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-    <title>Contact</title>
-
-</head>
-<body class="antialiased">
-<div class="container">
-    <h1>Contact page!</h1>
-</div>
-
-</body>
-</html>
+@section('content')
+    <h1>Contact page</h1>
+@endsection
--- laravel/resources/views/post.blade.php
+++ laravel/resources/views/post.blade.php
@@ -1,16 +1,5 @@
-<!DOCTYPE html>
-<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-<head>
-    <meta charset="utf-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1">
+@extends('layouts.app')
 
-    <title>Post</title>
-
-</head>
-<body class="antialiased">
-<div class="container">
-    <h1>Post {{$id}}</h1>
-</div>
-
-</body>
-</html>
+@section('content')
+    <h1>Post page</h1>
+@endsection
