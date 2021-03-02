f54a179d155d7dcfd26185ef74da876d8938ef7e
javorkabotond <botondjvorka@gmail.com>
2021-03-02 17:39:36 +0100

    Laravel-Migrations

--- laravel/resources/views/contact.blade.php
+++ laravel/resources/views/contact.blade.php
@@ -3,3 +3,4 @@
 @section('content')
     <h1>Contact page</h1>
 @endsection
+GRANT CREATE, ALTER, DROP, INSERT, UPDATE, DELETE, SELECT, REFERENCES, RELOAD on *.* TO 'javorkabotond'@'host' WITH GRANT OPTION;
